ca8f740fb8028cb69c8dbe4d2773ba750e73bfd0
jakoofficial <jacobvoandersen@gmail.com>
2020-04-12 16:46:13 +0200

    added jumping and fixed physics oh and the player is a prefab (remvoe the existing camera)

--- SimpleGameJam/PhewPhew/Assets/Scripts/PlayerMovement.cs
+++ SimpleGameJam/PhewPhew/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 4f;
+    public float jumpForce;
 
-    public Rigidbody2D rb;
+    private float hAxis;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public int jumpsLeft;
+
+    private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            jumpsLeft--;
+        }
+    }
+
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
         float hAxis = Input.GetAxis("Horizontal");
 
-        rb.velocity = new Vector2(hAxis * speed, 0f);
+        rb.velocity = new Vector2(hAxis * speed, rb.velocity.y);
     }
 }
