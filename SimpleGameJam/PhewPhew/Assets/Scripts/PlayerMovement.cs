415473983b0121cc5161c39e0193ffeed0fd7a8c
jakoofficial <jacobvoandersen@gmail.com>
2020-04-12 16:12:16 +0200

    did player movement but needs to change

---
+++ SimpleGameJam/PhewPhew/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 4f;
+
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(hAxis * speed, 0f);
+    }
+}
